dfb997dc190bc81e710623de676c80f846ffdff1
Søren Sejr Olsen <soerensejr@yahoo.dk>
2024-04-19 23:31:37 +0200

    disabling security and adding jenkins file

---
+++ Jenkinsfile
@@ -0,0 +1,2 @@
+
+springBootDockerCiPipeline()
--- src/main/java/ssols/foreveriot/configserver/ConfigServerApplication.java
+++ src/main/java/ssols/foreveriot/configserver/ConfigServerApplication.java
@@ -1,9 +1,12 @@
 package ssols.foreveriot.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ManagementWebSecurityAutoConfiguration.class
+		, SecurityAutoConfiguration.class})
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
